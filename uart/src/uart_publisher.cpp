97a292556bf929e66c452d12b8d34c17df9bd2cd
KAJ <jonas-kabelitz@gmx.de>
2025-04-11 02:00:32 +0200

    asdads

--- uart/src/uart_publisher.cpp
+++ uart/src/uart_publisher.cpp
@@ -7,42 +7,49 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <std_msgs/msg/u_int16.hpp>
+#include <geometry_msgs/msg/vector3.hpp>
 
 #include <boost/asio.hpp>
 
 #define layout_packed(Align) __attribute__((packed, aligned(Align)))
 
-struct layout_packed(1) test_payload{
-  std::uint32_t value;
-}; // struct test_payload
+// usbipd list
+// usbipd attach --wsl --busid 2-9
+// colcon build && source install/setup.sh && ros2 run uart uart_publisher
 
-struct layout_packed(1) package {
-  std::uint8_t start;
-  std::uint8_t size;
-  std::uint8_t type;
-  union layout_packed(1) {
-    test_payload test;
-  } payload;
-  std::uint8_t checksum;
-}; // struct package
-  
-inline static constexpr auto start_byte = std::uint8_t{0x7F};
-inline static constexpr auto escape_byte = std::uint8_t{0x7D};
-inline static constexpr auto escape_mask = std::uint8_t{0x20};
+struct layout_packed(1) vec3 {
+  std::float_t x;
+  std::float_t y;
+  std::float_t z;
+}; // struct vec3
 
 class uart_publisher : public rclcpp::Node {
 
   using base = rclcpp::Node;
 
-public:
+  inline static constexpr auto start_byte = std::uint8_t{0x7F};
+  inline static constexpr auto escape_byte = std::uint8_t{0x7D};
+  inline static constexpr auto escape_mask = std::uint8_t{0x20};
+
+  inline static constexpr auto rx_buffer_size = std::uint8_t{64u};
+
+  public:
 
   uart_publisher()
   : base{"uart_publisher"},
     _io_context{},
-    _serial_port{_io_context} {
+    _serial_port{_io_context},
+    _state{read_state::wait_start},
+    _package_index{0u},
+    _needs_escaping{false},
+    _expected_size{0u} {
     RCLCPP_INFO(base::get_logger(), "uart_publisher created");
 
-    _publisher = base::create_publisher<std_msgs::msg::String>("/uart/test_value", 10u);
+    _tof1_publisher = base::create_publisher<std_msgs::msg::UInt16>("/tof1", 10);
+    _tof2_publisher = base::create_publisher<std_msgs::msg::UInt16>("/tof2", 10);
+    _gyro_publisher = base::create_publisher<geometry_msgs::msg::Vector3>("/gyro", 10);
+    _accel_publisher = base::create_publisher<geometry_msgs::msg::Vector3>("/accel", 10);
 
     _serial_port.open("/dev/ttyUSB0");
 
@@ -52,52 +59,204 @@ public:
     _serial_port.set_option(boost::asio::serial_port::parity(boost::asio::serial_port::parity::none));
     _serial_port.set_option(boost::asio::serial_port::stop_bits(boost::asio::serial_port::stop_bits::one));
 
-    // boost::asio::async_read_until(_serial_port, boost::asio::dynamic_buffer(_read_buffer), 0x7D, [this](const boost::system::error_code& error_code, const std::size_t bytes_read) {
-    //   RCLCPP_INFO(base::get_logger(), "async_read_until uart");
-    //   _read_uart(error_code, bytes_read);
-    // });
-
-    // _io_context.run();
-
-    auto c = char{};
-
     while (true) {
-      boost::asio::read(_serial_port, boost::asio::buffer(&c, 1));
-      RCLCPP_INFO(base::get_logger(), "Byte: 0x%02X", c);
+      _update();
     }
   }
 
 private:
 
-  auto _echo(const boost::system::error_code& error_code, const std::size_t bytes_read) -> void {
+  enum class read_state : std::uint8_t {
+    wait_start,
+    read_size,
+    read_payload,
+    verify_checksum
+  }; // struct read_state
 
-  }
+  auto _update() -> void {
+    const auto bytes_read = boost::asio::read(_serial_port, boost::asio::buffer(_rx_buffer.data(), _rx_buffer.size()));
 
-  auto _read_uart(const boost::system::error_code& error_code, const std::size_t bytes_read) -> void {
-    if (error_code) {
-      RCLCPP_ERROR(base::get_logger(), "Error reading from UART: %s", error_code.message().c_str());
-      return;
-    }
+    for (auto i = 0u; i < bytes_read; ++i) {
+      _current_byte = _rx_buffer[i];
 
-    if (bytes_read != 10u) {
-      RCLCPP_ERROR(base::get_logger(), "Error reading from UART: %lu bytes read", bytes_read);
-      for (auto i = 0u; i < bytes_read; ++i) {
-        RCLCPP_INFO(base::get_logger(), "Byte %u: %02X", i, _read_buffer[i]);
+      if (!_decode()) {
+        continue;
+      }
+
+      switch (_state) {
+        case read_state::wait_start: {
+          if (_current_byte == start_byte) {
+            _package_index = 0u;
+            _state = read_state::read_size;
+          } else {
+            RCLCPP_WARN(base::get_logger(), "Unexpected byte: 0x%02X", _current_byte);
+          }
+
+          break;
+        }
+        case read_state::read_size: {
+          if (_current_byte == start_byte) {
+            RCLCPP_WARN(base::get_logger(), "Unexpected start byte 0x7E in read_size state");
+            _state = read_state::wait_start;
+
+            break;
+          }
+
+          // if (!_decode()) {
+          //   continue;
+          // }
+
+          _expected_size = _current_byte;
+          
+          if (_expected_size > _rx_buffer.size() - 1u) {
+            RCLCPP_WARN(base::get_logger(), "Invalid size byte 0x%02X exceeds buffer size", _current_byte);
+            _state = read_state::wait_start;
+          } else {
+            _state = read_state::read_payload;
+          }
+
+          break;
+        }
+        case read_state::read_payload: {
+          if (_current_byte == start_byte) {
+            RCLCPP_WARN(base::get_logger(), "Unexpected start byte 0x7F in payload");
+            _state = read_state::wait_start;
+            break;
+          }
+
+          if (!_decode()) {
+            continue;
+          }
+
+          _package_buffer[_package_index++] = _current_byte;
+
+          if (_package_index >= _expected_size) { 
+            _state = read_state::verify_checksum;
+          }
+        
+          break;
+        }
+        case read_state::verify_checksum: {
+          if (_current_byte == start_byte) {
+            RCLCPP_WARN(base::get_logger(), "Unexpected start byte 0x7F in checksum");
+            _state = read_state::wait_start;
+            break;
+          }
+
+          // if (!_decode()) {
+          //   continue;
+          // }
+
+          if (_verify_checksum({_package_buffer.data(), _package_index}, _current_byte)) {
+            _process_function({_package_buffer.data(), _expected_size});
+          } else {
+            RCLCPP_WARN(base::get_logger(), "Invalid checksum");
+          }
+
+          _state = read_state::wait_start;
+          break;
+        }
+        default: {
+          RCLCPP_WARN(base::get_logger(), "Invalid state");
+          _state = read_state::wait_start;
+          break;
+        }
       }
     }
 
-    _read_buffer.clear();
-
-    boost::asio::async_read_until(_serial_port, boost::asio::dynamic_buffer(_read_buffer), 0x7D, [this](const boost::system::error_code& error_code, const std::size_t bytes_read) {
-      _read_uart(error_code, bytes_read);
-    });
+    if (bytes_read == 0u && _state != read_state::wait_start) {
+      _state = read_state::wait_start;
+      RCLCPP_WARN(base::get_logger(), "Timeout: Incomplete package");
+    }
   }
 
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _publisher;
+  auto _process_function(std::span<const std::uint8_t> buffer) -> void {
+    const auto type = buffer[0];
+
+    switch (type) {
+      case 0x00: {
+        const auto* tof1 = reinterpret_cast<const std::uint16_t*>(buffer.data() + 1);
+        RCLCPP_INFO(base::get_logger(), "Received type 0x00, tof1: %u", *tof1);
+        auto msg = std_msgs::msg::UInt16{};
+        msg.data = *tof1;
+        _tof1_publisher->publish(msg);
+        break;
+      }
+      case 0x01: {
+        const auto* tof2 = reinterpret_cast<const std::uint16_t*>(buffer.data() + 1);
+        RCLCPP_INFO(base::get_logger(), "Received type 0x01, tof2: %u", *tof2);
+        auto msg = std_msgs::msg::UInt16{};
+        msg.data = *tof2;
+        _tof2_publisher->publish(msg);
+        break;
+      }
+      case 0x02: {
+        const auto* gyro = reinterpret_cast<const vec3*>(buffer.data() + 1);
+        RCLCPP_INFO(base::get_logger(), "Received type 0x02, vec3: (%f, %f, %f)", gyro->x, gyro->y, gyro->z);
+        auto msg = geometry_msgs::msg::Vector3{};
+        msg.x = gyro->x;
+        msg.y = gyro->y;
+        msg.z = gyro->z;
+        _gyro_publisher->publish(msg);
+        break;
+      }
+      case 0x03: {
+        const auto* accel = reinterpret_cast<const vec3*>(buffer.data() + 1);
+        RCLCPP_INFO(base::get_logger(), "Received type 0x03, vec3: (%f, %f, %f)", accel->x, accel->y, accel->z);
+        auto msg = geometry_msgs::msg::Vector3{};
+        msg.x = accel->x;
+        msg.y = accel->y;
+        msg.z = accel->z;
+        _accel_publisher->publish(msg);
+        break;
+      }
+      default: {
+        RCLCPP_WARN(base::get_logger(), "Unknown type 0x%02X", type);
+        break;
+      }
+    }
+  }
+
+  auto _verify_checksum(std::span<const std::uint8_t> buffer, std::uint8_t checksum) -> bool {
+    // Size is part of the packages checksum
+    auto calculated_checksum = static_cast<std::uint8_t>(buffer.size());
+
+    for (const auto byte : buffer) {
+      calculated_checksum ^= byte;
+    }
+
+    return calculated_checksum == checksum;
+  }
+
+  auto _decode() -> bool {
+    if (_current_byte == escape_byte) {
+      _needs_escaping = true;
+      return false;
+    }
+
+    if (_needs_escaping) {
+      _current_byte ^= escape_mask;
+      _needs_escaping = false;
+    }
+
+    return true;
+  }
+
+  rclcpp::Publisher<std_msgs::msg::UInt16>::SharedPtr _tof1_publisher;
+  rclcpp::Publisher<std_msgs::msg::UInt16>::SharedPtr _tof2_publisher;
+  rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr _gyro_publisher;
+  rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr _accel_publisher;
 
   boost::asio::io_context _io_context;
   boost::asio::serial_port _serial_port;
-  std::vector<std::uint8_t> _read_buffer;
+  
+  read_state _state;
+  std::array<std::uint8_t, rx_buffer_size> _rx_buffer;
+  std::array<std::uint8_t, 32u> _package_buffer;
+  std::size_t _package_index;
+  std::uint8_t _current_byte;
+  bool _needs_escaping;
+  std::uint8_t _expected_size;
 
 }; // class uart_publisher
 
