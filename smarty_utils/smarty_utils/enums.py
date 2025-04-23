f35cff0625dba3d313ecad886cff0b9308612c15
Super-T02 <75214791+Super-T02@users.noreply.github.com>
2025-04-23 08:14:09 +0200

    Added location enum

--- smarty_utils/smarty_utils/enums.py
+++ smarty_utils/smarty_utils/enums.py
@@ -53,3 +53,37 @@ class SIGNS(Enum):
     TURN_RIGHT = 16
     GIVE_WAY = 17
     PRIORITY = 18
+
+
+class Location(Enum):
+    """Location of the object."""
+
+    LEFT = "left"
+    RIGHT = "right"
+    OUTSIDE = "outside"
+    FRONT = "front"
+    BACK = "back"
+    UNKNOWN = "unknown"
+    NOT_RELEVANT = "not_relevant"
+
+    @staticmethod
+    def opposite(location: "Location") -> "Location":
+        """
+        Get the opposite location of a given location.
+
+        Arguments:
+            location -- Location to get the opposite of
+
+        Returns:
+            Location -- Opposite location
+        """
+        if location == Location.LEFT:
+            return Location.RIGHT
+        elif location == Location.RIGHT:
+            return Location.LEFT
+        elif location == Location.FRONT:
+            return Location.BACK
+        elif location == Location.BACK:
+            return Location.FRONT
+        else:
+            return location
